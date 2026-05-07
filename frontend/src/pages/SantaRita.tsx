e31f679bdf2abe6ea1e8617751c26ad6be9aa1f9
mateusribeirocampos <mateusrcampos@gmail.com>
2026-05-07 13:10:41 -0300

    Fix: images and pingsupabase.yml

--- frontend/src/pages/SantaRita.tsx
+++ frontend/src/pages/SantaRita.tsx
@@ -8,14 +8,14 @@ export default function SantaRita() {
       title: "Nascimento",
       description:
         "Santa Rita de Cássia nasceu por volta de 1381, em Roccaporena, um vilarejo localizado no sopé do Monte Rucino, no município de Cássia (Úmbria, Itália). Sinal das Abelhas brancas. Após a solenidade de seu batismo, Rita foi deixada para dormir. Quando seus pais foram observá-la, viram abelhas brancas voando em seu redor, pousando sobre seus lábios, como que depositando mel.",
-      image: "assets/birth.jpg",
+      image: "/assets/birth.jpg",
     },
     {
       year: "1398-1399",
       title: "Infância, juventude e casamento",
       description:
         "Rita teria desejado ser monja, todavia ainda jovem (aos 13 anos) os pais, já idosos, a prometeram em casamento a Paulo Ferdinando Mancini, um homem conhecido pelo seu caráter iroso e brutal. Santa Rita, habituada ao dever não opôs resistência e se casou com o jovem oficial que comandava a guarnição de Collegiacone, presumivelmente entre os 17-18 anos",
-      image: "assets/monja.jpg",
+      image: "/assets/monja.jpg",
     },
     {
       year: "1406-1407",
@@ -23,28 +23,28 @@ export default function SantaRita() {
       description:
         "A insistência de suas lágrimas frutificou. Vários biógrafos indicam que aos poucos o temperamento de Paolo mudou substancialmente. Soma-se a isso, o nascimento dos dois filhos do casal: Giangiacomo Antonio e Paolo Maria. Aparentemente, eram dias de harmonia e esperança. Mas o pecado sempre cobra seu salário! E na história de Rita, foi a esposa e mãe exemplar quem teve que sacrificar seu tesouro espiritual. Sua serenidade doméstica não duraria, no entanto, devido à implacável luta faccional da época, na qual o marido de Rita estava envolvido devido a seus laços de parentesco foi assassinado. Para evitar que as crianças procurassem vingança, Rita escondeu a camisa ensanguentada de seu pai.",
       image:
-        "assets/ritafilhos.jpg",
+        "/assets/ritafilhos.jpg",
     },
     {
       year: "1407-1413",
       title: "Rita e o mosteiro",
       description: "Celebração de Rita procurou as Irmãs Agostinianas do Mosteiro de S. Maria Madalena, em Cássia. Em virtude do histórico violento de seu esposo e das rixas entre seus cunhados e a família dos assassinos, foi-lhe negado o ingresso por três vezes, sendo-lhe permitido somente após uma reconciliação pública entre as famílias. Uma antiga tradição assegura que sua entrada no Mosteiro foi miraculosa: Rita estaria em oração na “rocha”, e foi tomada por seus três santos padroeiros (Santo Agostinho, S. João Batista e São Nicolau de Tolentino) que a transportaram pelos ares para Cássia, introduzindo-o no interior do mosteiro; quando as irmãs a viram, apesar das portas fechadas, convencidas pelo sinal a acolherem.",
       image:
-        "assets/ritatressantos.jpg",
+        "/assets/ritatressantos.jpg",
     },
     {
       year: "1442-1457",
       title: "Frutos",
       description: "A fim de aprimorar, e provar, o dom da obediência, a Madre Priora pediu que Rita regasse diariamente uma planta seca no jardim. Para surpresa de todos, Rita obedeceu com tamanha perfeição que aquele toco sem vida floresceu e frutificou. Segundo algumas fontes, brotou uma ameixeira, mas para uma antiga tradição, tal planta é a videira que ainda existe, depois de cinco séculos, no Mosteiro de Cássia.",
       image:
-        "assets/videira.jpg",
+        "/assets/videira.jpg",
     },
     {
       year: "1446-1457",
       title: "O estigma",
       description: "Se não bastasse todo o sofrimento que a vida lhe ofertara, Rita ansiava por viver na carne uma parcela das dores vividas por Cristo em sua paixão. Este desejo foi a causa de muitas de suas orações, e sua prece foi atendida em 1432: enquanto contemplava o Crucifixo, sentiu um espinho da coroa de Cristo cravar em sua testa, produzindo uma ferida profunda. Para muitos biógrafos, tal ferida era dolorosíssima, com constante sangramento, e expelia um odor pútrido, obrigando Rita a viver isolada das demais religiosas. Tal ferida só desapareceu momentaneamente por ocasião de uma peregrinação à Roma, para a canonização de São Nicolau de Tolentino, em 1446.",
       image:
-        "assets/estigma.jpg",
+        "/assets/estigma.jpg",
     },
   ];
 
